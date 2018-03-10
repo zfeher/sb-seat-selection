9538e1d9410ccaeb1703f564ecd31e67fb86a10f
Zoltan Feher <zf_common@outlook.com>
2018-03-10 17:36:30 +0100

    feat(base-seat): add skeleton and wire in

---
+++ src/app.vue
@@ -0,0 +1,40 @@
+<template>
+  <div 
+    id="app" 
+    :class="$style.app"
+  >
+    <BaseSeat 
+      :theme="undefined"
+      :mark="null" 
+    />
+  </div>
+</template>
+
+<script>
+import BaseSeat from './components/base-seat.vue'
+import { SEAT_THEMES } from './constants'
+
+export default {
+  name: 'App',
+
+  components: {
+    BaseSeat,
+  },
+
+  data() {
+    return {
+      seatThemes: SEAT_THEMES,
+    }
+  },
+}
+</script>
+
+<style module>
+.app {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 16px); /* to hide scrollbar */
+}
+</style>
---
+++ src/components/base-seat.vue
@@ -0,0 +1,37 @@
+<template>
+  <div :class="$style.background">
+    <span :class="$style.mark">Base Seat</span>
+  </div>
+</template>
+
+<script>
+import { SEAT_THEME_ASSIGNED } from '../constants'
+import { seatMarkValidator } from '../prop-validators/seat-mark-validator'
+import { seatThemeValidator } from '../prop-validators/seat-theme-validator'
+
+export default {
+  props: {
+    mark: {
+      type: String,
+      default: 'x',
+      validator: seatMarkValidator,
+    },
+
+    theme: {
+      type: Object,
+      default: () => SEAT_THEME_ASSIGNED,
+      validator: seatThemeValidator,
+    },
+  },
+}
+</script>
+
+<style module>
+.background {
+  background-color: cadetblue;
+}
+
+.mark {
+  color: beige;
+}
+</style>
