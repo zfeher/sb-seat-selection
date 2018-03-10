40c2fd8965eb57de196cf9dbace9bc222a175bc6
Zoltan Feher <zf_common@outlook.com>
2018-03-10 17:35:28 +0100

    feat(constants): add seat theme constants

---
+++ src/constants.js
@@ -0,0 +1,26 @@
+export const SEAT_THEME_AVAILABLE = {
+  backgroundColor: '#080',
+  textColor: '#fff',
+}
+
+export const SEAT_THEME_SELECTED = {
+  backgroundColor: '#dd0',
+  textColor: '#fff',
+}
+
+export const SEAT_THEME_COMPANION = {
+  backgroundColor: '#088',
+  textColor: '#fff',
+}
+
+export const SEAT_THEME_ASSIGNED = {
+  backgroundColor: '#888',
+  textColor: '#fff',
+}
+
+export const SEAT_THEMES = [
+  SEAT_THEME_ASSIGNED,
+  SEAT_THEME_AVAILABLE,
+  SEAT_THEME_COMPANION,
+  SEAT_THEME_SELECTED,
+]
